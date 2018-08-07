7ed2c515ea41e4c251369bbf6bae2d2bed844ee5
kono <konotorie@gmail.com>
2018-08-07 23:52:02 +0900

    add gitignore

--- tryFlask.py
+++
@@ -1,9 +0,0 @@
-#flask:http://flask.pocoo.org/docs/1.0/
-#set exporting the FLASK_APP="this file name" in your terminal environment.
-#then command "flask run". A server starts.
-from flask import Flask
-app = Flask(__name__) #__name__ is a name of this file(module) when you declare it in another module.
-
-@app.route("/")
-def hello_world():
-    return "hello world"
